47ee00d3d9f3053b45c36003dc0806796b7c084c
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2023-06-26 20:58:37 -0700

    update to clicker

--- Sticks_and_Stones.gd
+++ Sticks_and_Stones.gd
@@ -10,19 +10,20 @@ func _ready():
 
 
 
-
-
-
-
-func _on_spawn_timer_timeout():
-	if rng.randi_range(0,100) <= 60:
-		var stick = Stick.instantiate()
-		add_child(stick)
-		stick.global_position = Vector2(rng.randi_range(25,1127), rng.randi_range(50,598))
-		stick.rotation = rng.randi_range(0,360)
+func _on_stone_spawn_timer_timeout():
 	if rng.randi_range(0,100) <= 30:
 		var stone = Stone.instantiate()
 		add_child(stone)
 		stone.global_position = Vector2(rng.randi_range(25,1127), rng.randi_range(50,598))
 		stone.rotation = rng.randi_range(0,360)
-	
+	$Stone_spawn_timer.wait_time = rng.randf_range(0.25,1.5)
+	$Stone_spawn_timer.start()
+
+func _on_stick_spawn_timer_timeout():
+	if rng.randi_range(0,100) <= 60:
+		var stick = Stick.instantiate()
+		add_child(stick)
+		stick.global_position = Vector2(rng.randi_range(25,1127), rng.randi_range(50,598))
+		stick.rotation = rng.randi_range(0,360)
+	$Stick_spawn_timer.wait_time = rng.randf_range(0.2,1.25)
+	$Stick_spawn_timer.start()
