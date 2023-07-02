c86304290e55aeedc0b2cab1d1ee2d4f39587d4c
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2023-07-01 17:41:30 -0700

    stick and stones update
    texture rect instead of sprite. git rid of not needed code/ make it cleaner, Marker 2D in middle of sprite panel

--- stick_button.gd
+++ stick_button.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends TextureButton
 
 @onready var mouse = get_parent().get_node("Mouse_area")
 var clicked = false
@@ -8,17 +8,16 @@ var stick_label_position
 func _physics_process(delta):
 	if $Area2D.overlaps_area(mouse) == true:
 		if Global.Stick_Stones_hover_mode == true:
-			stick_label_position = get_parent().get_node("Stick_panel/TextureRect").global_position
+			stick_label_position = get_parent().get_node("Stick_panel/Marker2D").global_position
 			clicked = true
-			$Stick_button.visible = false
-			$Stick_button.disabled = true
+			disabled = true
 			$AnimationPlayer.play("size")
 	
 	if clicked == true:
 		if speed <= 12:
 			speed *= 1.05
 		global_position += Vector2(stick_label_position - global_position).normalized() * speed
-		if get_parent().get_node("Stick_panel/TextureRect").global_position.distance_to(global_position) <= 70:
+		if stick_label_position.distance_to(global_position) <= 50:
 			Global.sticks += 1
 			queue_free()
 
@@ -27,14 +26,9 @@ func _on_disappear_timer_timeout():
 		queue_free()
 
 
-func _on_stick_button_button_down():
+func _on_button_down():
 	if Global.Stick_Stones_hover_mode == false:
-		stick_label_position = get_parent().get_node("Stick_panel/TextureRect").global_position
+		stick_label_position = get_parent().get_node("Stick_panel/Marker2D").global_position
 		clicked = true
-		$Stick_button.visible = false
-		$Stick_button.disabled = true
+		disabled = true
 		$AnimationPlayer.play("size")
-
-
-
-
